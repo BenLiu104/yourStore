0030ca84f1d780d11bb0b4caa1dc68cdaee55eaa
BenLiu <liumanpan1990@hotmail.com>
2023-04-08 16:42:21 -0700

    update pipeline

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'yourstore-db.mysql.database.azure.com',
+      host: 'yourstore-mysqldb.mysql.database.azure.com',
       dialect: 'mysql',
       port: 3306,
     }
